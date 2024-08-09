fc0a125b55598500a7b36f3efc32169de7c6f67f
lapael1 <lapael6@gmail.com>
2024-08-09 11:31:09 +0900

    홈플러스

--- js/tabbtn.js
+++ js/tabbtn.js
@@ -1,32 +1,25 @@
-// / 1. 탭 버튼과 탭 내용 부분들을 querySelectorAll을 사용해 변수에 담는다.
-const tabItem = document.querySelectorAll(".tab__item");
-const tabContent = document.querySelectorAll(".tab__content");
-
-// 2. 탭 버튼들을 forEach 문을 통해 한번씩 순회한다.
-// 이때 index도 같이 가져온다.
-tabItem.forEach((item, index) => {
-  // 3. 탭 버튼에 클릭 이벤트를 준다.
-  item.addEventListener("click", (e) => {
-    // 4. 버튼을 a 태그에 만들었기 때문에, 
-    // 태그의 기본 동작(링크 연결) 방지를 위해 preventDefault를 추가한다.
-    e.preventDefault(); // a 
-    
-    // 5. 탭 내용 부분들을 forEach 문을 통해 한번씩 순회한다.
-    tabContent.forEach((content) => {
-      // 6. 탭 내용 부분들 전부 active 클래스를 제거한다.
-      content.classList.remove("active");
+window.addEventListener("load", function () {
+  const tabtitle = this.document.querySelectorAll(".refresBar");
+  tabtitle.forEach(function (item) {
+    // console.log(item);
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      //   메뉴탭에 화살표 활성기능
+      tabtitle.forEach(function (element) {
+        // console.log(element);
+        element.classList.remove("active");
+      });
+      this.classList.add("active");
+      //  메뉴탭에 해당하는 섹션내용이 바뀌는 코드
+      const tabItem = document.querySelectorAll(".tabitem");
+      tabItem.forEach(function (tab) {
+        tab.style.display = "none";
+      });
+      console.log(this);
+      const target = this.getAttribute("href");
+      document.querySelector(target).style.display = "block";
     });
-
-    // 7. 탭 버튼들을 forEach 문을 통해 한번씩 순회한다.
-    tabItem.forEach((content) => {
-      // 8. 탭 버튼들 전부 active 클래스를 제거한다.
-      content.classList.remove("active");
-    });
-
-    // 9. 탭 버튼과 탭 내용 영역의 index에 해당하는 부분에 active 클래스를 추가한다.
-    // ex) 만약 첫번째 탭(index 0)을 클릭했다면, 같은 인덱스에 있는 첫번째 탭 내용 영역에
-    // active 클래스가 추가된다.
-    tabItem[index].classList.add("active");
-    tabContent[index].classList.add("active");
   });
+  //   사이트가 로드됐을때 기본값(활성화된 값)
+  tabtitle[0].click();
 });
